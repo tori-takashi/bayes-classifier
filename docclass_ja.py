6f1ad9e01bff9a98fda560a086d05484dae849bb
bobbobfruits <aporia2718@outlook,com>
2018-11-19 23:35:56 +0900

    docclass en&ja

---
+++ docclass_ja.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from beautifulsoup4 import BeautifulSoup
+import sys
+
+app_id = "dj0zaiZpPVd3OE52UEJhZlRVTSZzPWNvbnN1bWVyc2VjcmV0Jng9ZGI-"
+page_url = "https://jlp.yahooapis.jp/MAService/V1/parse"
+
+def split( sentence, app_id=app_id, results="ma", filter='1|2|4|5|9|10'):
+    ret = []
+    sentence = quote_plus(sentence.encode("utf-8"))
+    query = "%s?appid=%s&results=%s&uniq_filter=%s&sentence=%s" % \
+            (pageurl,app_id,results,filter,sentence)
+    soup = BeautifulSoup(urlopen(query))
+    try:
+        return [l.surface.string for l in soup.ma_result.word_list]
+    except:
+        return []
+
+split(sys.argv[1])
